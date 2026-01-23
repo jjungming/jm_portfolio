c1aa74b921162816fc1a7408affb57903140a901
JJungMing <1010pjm@naver.com>
2026-01-24 02:05:05 +0900

    archiving firtst settings

--- src/components/sections/Archiving/Archiving.jsx
+++ src/components/sections/Archiving/Archiving.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import styles from './Archiving.module.css';
+import SectionTitle from '../../common/SectionTitle/SectionTitle';
 
+import GitImg from '../../../img/Archiving/github-icon-remove.png';
+
+/* GitHub, Blog, Notion, Resume 링크 제공 */
 const Archiving = () => {
   return (
-    <section className={styles.archiving}>
-      {/* GitHub, Blog, Notion, Resume 링크 제공 */}
+    <section id="archiving" className={styles.archivingContainer}>
+      
+      <div className={styles.archivingContent}>
+        
+        {/* 타이틀 */}
+        <SectionTitle title='Archiving' />
+
+        {/* 링크 제공 */}
+        <a className={styles.archivingWrapper} href='https://github.com/JJungMing'>
+          <div className={styles.archivingItems}>
+            <div className={styles.archivingImgWrapper}><img src={GitImg} alt="GitHub 이미지" className={styles.archivingImg} /></div>
+            <div className={styles.archivingURL}>https://github.com/JJungMing</div>
+            <div className={styles.archivingDescription}>소스코드 저장소</div>
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
