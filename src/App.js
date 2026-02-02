e414bdf1d55b5199ac936ed0558cca3e11880d33
JJungMing <1010pjm@naver.com>
2026-02-02 22:57:50 +0900

    add footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from '../src/components/sections/About/About';
 import Skills from '../src/components/sections/Skills/Skills';
 import Archiving from '../src/components/sections/Archiving/Archiving';
 import Projects from '../src/components/sections/Projects/Projects';
+import Footer from '../src/pages/footer/footer';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
       <section id="skills"><Skills /></section>
       <section id="archiving"><Archiving /></section>
       <section id="projects"><Projects /></section>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.appfooter}>
+        ⓜ 2026. Park Jung Min. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
